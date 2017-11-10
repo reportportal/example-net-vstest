f486a2e80cfa8d61329d72d25ca0aab38c48804d
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2017-11-10 15:16:00 +0300

    net 46

---
+++ MsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("MsTests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f0fd232-b8ac-4d32-ad94-bc57978d3732")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
