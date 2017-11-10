32348be25e5e0017103117eb91b710751b612e0b
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2017-11-10 14:14:03 +0300

    Adedd simple ms tests

---
+++ ExampleMsTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExampleMsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+    }
+}
---
+++ ExampleMsTests/UnitTest2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExampleMsTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Console output from UnitTest2 - TestMetod1");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Console.WriteLine("Console output from UnitTest2 - TestMetod2");
+        }
+    }
+}
