8afabed0dfc6c7d7ebb4e91a1aee5d7b652ea89f
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2018-09-26 19:56:17 +0300

    Revert "Remove old project"
    This reverts commit 231c4cff1446a7d14a4d68c48f4d1b5cbf0d8931.

---
+++ ExampleTestsOld/UnitTest1.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReportPortal.Shared;
+using System;
+
+namespace ExampleUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Bridge.LogMessage(ReportPortal.Client.Models.LogLevel.Info, "Message from logger");
+            Console.WriteLine("Console output from UnitTest2 - TestMetod1");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Console.WriteLine("Console output from UnitTest2 - TestMetod2");
+        }
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            TestContext.AddResultFile("cat.png");
+        }
+    }
+}
