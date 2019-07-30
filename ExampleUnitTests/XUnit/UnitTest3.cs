a0fe5708354cf0da1851936cbea33559d29697d2
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-07-30 15:51:16 +0300

    Add xunit tests

---
+++ ExampleUnitTests/XUnit/UnitTest3.cs
@@ -0,0 +1,16 @@
+using ReportPortal.Shared;
+using System;
+using System.Collections;
+using Xunit;
+
+namespace ExampleUnitTests.XUnitTests
+{
+    public class UnitTest3
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
